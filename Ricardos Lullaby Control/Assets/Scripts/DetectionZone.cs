63b92b56375401dc9f600d82fb56cb8872f5366b
LuisGomes-Obvio <a31473@alunos.ipca.pt>
2026-05-24 23:58:46 +0100

    Dicas e Velocidade Aumentada

--- Ricardos Lullaby Control/Assets/Scripts/DetectionZone.cs
+++ Ricardos Lullaby Control/Assets/Scripts/DetectionZone.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GoalZone : MonoBehaviour
 {
     public int requiredObjects = 6; // número de objetos para ganhar
     public GameObject winScreen;
     private HashSet<GameObject> objectsInZone = new HashSet<GameObject>();
+    public EnemyControl enemy;
+    public float speedIncrease = 0.5f;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,6 +17,9 @@ public class GoalZone : MonoBehaviour
             if (!objectsInZone.Contains(other.gameObject))
             {
                 objectsInZone.Add(other.gameObject);
+
+                   if (enemy != null)
+                enemy.GetComponent<NavMeshAgent>().speed += speedIncrease;
 
                 Debug.Log("Objetos na zona: " + objectsInZone.Count);
 
